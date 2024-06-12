4283b11f47a1645faf1dd1287e61b120b4af8f11
Romans Maksjutovs <maksjutovs@gmail.com>
2024-06-12 22:11:17 +0200

    Add @Input and @Output

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output,} from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const ramdomUser = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -7,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string
+  @Input({required: true}) avatar!: string
+  @Input({required: true}) name!: string
+  @Output() select = new EventEmitter<string>()
+  
 
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar
+  }
+
+
+  onSelectUser() {
+    this.select.emit(this.id)
+  }
 }
